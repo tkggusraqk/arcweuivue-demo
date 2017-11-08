fa99ee34e08169cbf6a930f087713e0ae75b8698
jing cheng <xinjingcheng@outlook.com>
2017-11-08 21:02:18 +0800

    publish

--- dist/js/10.b02cdf92a42c650c2fd1.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([10],{Rzht:function(t,e,n){"use strict";var u=function(){var t=this,e=t.$createElement;return(t._self._c||e)("div",[t._v("\n  testvue\n")])},r=[],a={render:u,staticRenderFns:r};e.a=a},b6pE:function(t,e,n){"use strict";e.a={data:function(){return{}}}},ua3t:function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var u=n("b6pE"),r=n("Rzht"),a=n("VU/8"),s=a(u.a,r.a,!1,null,null,null);e.default=s.exports}});
-//# sourceMappingURL=10.b02cdf92a42c650c2fd1.js.map
---
+++ dist/js/23.2fea03aaf22d3639e1ab.js
@@ -0,0 +1,2 @@
+webpackJsonp([23],{"4IP3":function(t,e,n){"use strict";var r=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",[n("Badge",{attrs:{text:"123"}}),t._v(" "),n("br"),t._v(" "),n("Badge",{attrs:{isDot:!0}})],1)},a=[],s={render:r,staticRenderFns:a};e.a=s},"6CWX":function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var r=n("RkRv"),a=n("4IP3"),s=n("VU/8"),u=s(r.a,a.a,!1,null,null,null);e.default=u.exports},RkRv:function(t,e,n){"use strict";e.a={data:function(){return{}}}}});
+//# sourceMappingURL=23.2fea03aaf22d3639e1ab.js.map
