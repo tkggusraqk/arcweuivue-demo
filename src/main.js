c22301222fa55eb14f73b2603c539ea15dbac97c
jing cheng <xinjingcheng@outlook.com>
2017-11-09 12:28:25 +0800

    修复tag 不显示移除图标问题

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-// import ArcWeUi from '@/index.js'
 import ArcWeUI from 'arcweui-vue'
 import 'arcweui-vue/dist/arcweui-vue.min.css'
 
